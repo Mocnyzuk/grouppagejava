6ef25e02a3d28b647fa4694f96cab3dc3bbbaa0d
mocnyzuk <jakub.jureczko@interia.pl>
2020-09-11 22:51:52 +0200

    another good fix

--- src/main/java/com/grouppage/service/ChatService.java
+++ src/main/java/com/grouppage/service/ChatService.java
@@ -92,7 +92,7 @@ public class ChatService {
             List<ParticipantLight> pLights = Stream.of(pierwszy, drugi).map(ParticipantLight::fromParticipant).collect(Collectors.toList());
             System.out.println(Arrays.toString(userIds.toArray()));
             all.forEach(part -> this.sendMessageOrPost(part.getUser().getId(),
-                    new ConversationMessage(part.getUser().getId() == (pierwszy.getUser().getId()) ? drugi.getUser().getId() : pierwszy.getUser().getId(),
+                    new ConversationMessage(part.getUser().getId() == (pierwszy.getUser().getId()) ? drugi.getId() : pierwszy.getId(),
                             conv.getId(), pLights, socketMessage.getType())));
 
         }else{
