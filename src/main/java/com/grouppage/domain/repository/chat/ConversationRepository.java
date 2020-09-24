bf240fc000225d98514c03cc839a483971461ccd
Mocnyzuk <56972105+Mocnyzuk@users.noreply.github.com>
2020-09-24 15:16:04 +0200

    Update ConversationRepository.java
    import @param

--- src/main/java/com/grouppage/domain/repository/chat/ConversationRepository.java
+++ src/main/java/com/grouppage/domain/repository/chat/ConversationRepository.java
@@ -5,6 +5,7 @@ import com.grouppage.domain.entity.User;
 import com.grouppage.domain.entity.chat.Conversation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
